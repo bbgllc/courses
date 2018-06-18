d652baef86b85114b776a7f9537b34b7a1ff58f2
Jason-guru <jobmails1689@gmail.com>
2018-06-18 17:08:17 +0530

    paypal credential removed

--- routes/web.php
+++ routes/web.php
@@ -48,29 +48,3 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function(){
 Route::get('/config-cache', function(){
 	Artisan::call('config:cache');
 });
-
-Route::get('/paypal-test', function(){
-    $ch = curl_init();
-    $clientId = "AQYVHG3vLhtTAE3T12wf_TU0MhCOZrSoLIFBuhhV9aBda3pfwWF6VCT5M8iXzvVGInsMW2D7hr1MHsYK";
-    $secret = "EHChLLKEhSom3zgnTR3T7Cbrymm1CqLqiteA7mPb_UrYxIVtg3B-HqkH-oTYJXU59xDwDgjVANS84v-Q";
-
-    curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/oauth2/token");
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    curl_setopt($ch, CURLOPT_USERPWD, $clientId.":".$secret);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
-
-    $result = curl_exec($ch);
-
-    if(empty($result))die("Error: No response.");
-    else
-    {
-        $json = json_decode($result);
-        print_r($json);
-    }
-
-    curl_close($ch);
-});
-
