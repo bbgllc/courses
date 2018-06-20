3c71d6cda5c01d833e751166c8b9c9e4e69ebb64
Jason-guru <jobmails1689@gmail.com>
2018-06-20 23:08:14 +0530

    Footer text and url chenged

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -3,5 +3,5 @@
         <a href="/">{{config('site_settings.site_name')}}</a>
     </strong> {{ __('strings.backend.general.all_rights_reserved') }}
 
-    <span class="float-right">Powered by <a href="https://supportcube.io" target="_blank">EduCore</a></span>
+    <span class="float-right">Powered by <a href="https://burnsdigital.co" target="_blank">Burns Digital Co.</a></span>
 </footer>
