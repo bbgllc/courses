794d6be787da8e539c5cecb7e0d797b8d7e3f461
Jason-guru <jobmails1689@gmail.com>
2018-07-11 16:40:44 +0530

    view demo data

--- Themes/default/views/_Auth/login.blade.php
+++ Themes/default/views/_Auth/login.blade.php
@@ -70,15 +70,15 @@
                                     </div>
                                     <code class="d-block w-100">
                                         <div class="col-12 text-left mb-2">
-                                            Email: admin@burnsdigital.co <br />
+                                            Email: admin@burnsdigital.co <br/>
                                             Password: password <br />
                                         </div>
                                         <div class="col-12 text-left mb-2">
-                                            Email: author@burnsdigital.co <br />
+                                            Email: author@burnsdigital.co <br/>
                                             Password: password <br />
                                         </div>
                                         <div class="col-12 text-left">
-                                            Email: user@burnsdigital.co <br />
+                                            Email: user@burnsdigital.co <br/>
                                             Password: password <br />
                                         </div>
                                     </code>
